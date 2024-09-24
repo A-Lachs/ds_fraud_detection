f00b9a0a2ad757859a120c9e72a873b51f463f12
Lachs <lachspost@gmail.com>
2024-09-24 18:00:45 +0100

    updating plotting functions for monthly consumption

--- Scripts/plotting.py
+++ Scripts/plotting.py
@@ -251,9 +251,9 @@ def subplopts_fraud_per_category(your_df:pd.DataFrame,
     return grouped_df, fraud_risk_categories
 
 
-#######################################
-### Boxplots for energy consumption ###
-#######################################
+###################################################
+### 1x4 grid of boxplots for energy consumption ### - grouped by consumption level and target
+###################################################
 
 
 def boxplot_consumption_per_level(data:pd.DataFrame, energy_type: str, feature: str, show_outliers=True):
@@ -317,6 +317,165 @@ def boxplot_consumption_per_level(data:pd.DataFrame, energy_type: str, feature: 
     # tight_layout only considers ticklabels, axis labels, and titles
     # adjustment with [left bottom, right, top] in normalized (0,1) space
     # increase top value to reduce space between top figures and suptitle (that is created when customizing the legend)
+    
+    plt.show()
+    return
+
+
+############################################################
+### 2x2 grid of lineplots for montly energy consumption  ### - grouped by consumption level and target
+############################################################
+
+#  main function plot_monthly_consumption() for plotting levelwise monthly consumption grouped by target
+#  uses subfunctions aggregate_monthly_consumption() and calculate_error_bar_bounds() to draw each subplot
+
+
+def aggregate_monthly_consumption(data: pd.DataFrame, energy_type: str, consumption_level: int) -> pd.DataFrame:
+    """ For plotting aggregate the mean monthly consumption data over all clients for a given energy type ('elec' or 'gas') 
+        and level (1, 2, 3 or 4) grouped by month and by target (fraud or no fraud). 
+
+    Args:
+        data (pd.DataFrame):        DF that has for each client (rows) columns with levelwise mean monthly consumption data
+                                    and a column 'target' (with codes 1 and 0 for fraud/ no fraud)
+        energy_type (str):          Has to be 'elec' or 'gas'
+        consumption_level (int):    Consumption data are available for level 1 to 4.
+
+    Returns:
+        pd.DataFrame:               Df with columns 'target', 'months' and aggregated features (mean, std, max_min_range) 
+    """
+
+    operations = ['mean', 'std', max_min_range]     # means of  aggregation 
+    aggregations = {}
+
+    for month in range(1,13):
+        aggregations[f'{energy_type}_{consumption_level}_mon_{month}_mean'] = operations
+        agg_data = data.groupby('target', observed=False).agg(aggregations)
+   
+    # change format for plotting (using stack)
+    # agg_data = agg_data.stack(level=0, future_stack=True).reset_index()
+    agg_data = agg_data.stack(level=0).reset_index()
+
+    # rename months  
+    agg_data.rename(columns={'level_1': 'months'}, inplace=True)
+    agg_data['months'] = [i.split('_')[3] for i in agg_data['months'] ]
+
+    # convert month dtype to ordered category for plotting
+    agg_data['months'] = pd.Categorical(agg_data['months'],
+                                       categories=[str(i) for i in list(range(1,13))], 
+                                       ordered=True
+                                       )
+
+    return agg_data
+
+
+def calculate_error_bar_bounds(y: pd.Series, error: pd.Series, limit=True)->tuple:
+    """ From the input y values and corresponding errors compute the lower and upper bound 
+        and return it as a tuple (lower, upper). 
+
+    Args:
+        y (pd.Series):          y_values, example: data[data.target == 0]['mean']
+        error (pd.Series):      Corresponding error values data[data.target == 0]['std']
+        limit (bool, optional): Limit the bounds to positive values. Defaults to True.
+
+    Returns:
+        tuple:                  Values vor lower, upper boundries.
+    """
+
+    # y_data = data[data.target == 0]['mean']
+    # error = data[data.target == 0]['std']
+    upper = y + error
+    lower = y - error 
+    if limit: #  set negative values to zero for plotting
+        lower.loc[lower <0 ] = 0 
+    return lower, upper
+
+
+def plot_monthly_consumption(data_df: pd.DataFrame, energy_type: str, metric: str, error_metric='std'):
+    """ In a 2x2 grid of subplots display the energy consumption (of a specific type 'elec' or 'gas') 
+        for each level 1 to 4. Data is gropued by month and target (fraud/ no fraud).
+
+    Args:
+        data_df (pd.DataFrame):         DF that has for each client (rows) columns with levelwise mean monthly consumption data
+                                        and a column 'target' (with codes 1 and 0 for fraud/ no fraud)
+        energy_type (str):              Has to be 'elec' or 'gas'.
+        metric (str):                   Metric used for data aggregation: 'mean'
+        error_metric (str, optional):   Metric used to create error bands. Defaults to 'std'.
+    """
+    #  energy type 'elec' or 'gas' has to be chosen
+
+    if energy_type == 'elec':
+        consumption_label = 'electricity'
+    elif energy_type == 'gas':
+        consumption_label = energy_type
+    else:
+        return f'Energy type is not any of "elec" or "gas"'
+    
+    # construct the plot 
+
+    fig, axes = plt.subplots(2, 2, figsize=(8, 7), sharex=True, sharey=True)
+    fig.suptitle(f'Monthly {consumption_label} consumption', fontsize=14, verticalalignment='center')
+
+    axes_ref = ([0, 0], [0, 1], [1, 0], [1, 1]) # to address the axes of each subplot in a 2x2 grid
+    level_descriptions = ['< 2.400 kWh', 
+                          '2.401 - 3.600 kWh', 
+                          '3.401 - 6.000 kWh', 
+                          '> 6.000 kWh',]
+
+    for subplot in range(0,4):
+
+        # aggregate the data
+        data = aggregate_monthly_consumption(data_df, energy_type, consumption_level=subplot+1)
+
+        sns.lineplot(ax=axes[axes_ref[subplot][0]][axes_ref[subplot][1]], 
+                    data=data, x='months', y=metric, 
+                    hue='target', palette=[COLOR_1, RED_COLORS[1]], 
+                    linewidth=2,
+                    )
+        
+        # change subplot titles/labels
+        axes[axes_ref[subplot][0]][axes_ref[subplot][1]].set_title(f'Consumption level {subplot+1}', fontsize=9)
+        if error_metric:
+            axes[axes_ref[subplot][0]][axes_ref[subplot][1]].set_ylabel(f'{metric.title()} ({error_metric}) consumption [kWh]')
+        else:
+            axes[axes_ref[subplot][0]][axes_ref[subplot][1]].set_ylabel(f'{metric.title()} consumption [kWh]')
+        axes[axes_ref[subplot][0]][axes_ref[subplot][1]].set_xlabel('Months')
+    
+        # remove legends
+        axes[axes_ref[subplot][0]][axes_ref[subplot][1]].legend([],[], frameon=False)  
+
+        # add custom legend to specific subplot (and dont' forget to adjust plt.tight_layout() accortdingly!)
+        if subplot == 1:                   
+            legend_handles, _= axes[axes_ref[subplot][0]][axes_ref[subplot][1]].get_legend_handles_labels()
+            axes[axes_ref[subplot][0]][axes_ref[subplot][1]].legend(legend_handles, ['normal', 'fraud'])
+            sns.move_legend(axes[axes_ref[subplot][0]][axes_ref[subplot][1]], 
+                            title='',       # remove legend title
+                            bbox_to_anchor=(1.02, 1.29), 
+                            loc='upper right')  
+        
+        ### Optional:  Add custom error bars for each group 
+
+        if error_metric:    
+            
+            # calculate error bar bounds
+            normal_error = calculate_error_bar_bounds(y=data[data.target == 0][metric],
+                                                    error=data[data.target == 0][error_metric])
+            fraud_error = calculate_error_bar_bounds(y=data[data.target == 1][metric], 
+                                                    error=data[data.target == 1][error_metric])
+            
+            #x = data['months'].unique() # does not have the correct order
+            x=[str(i) for i in list(range(1,13))]
+
+            #  fill space in between upper and lower error range
+            axes[axes_ref[subplot][0]][axes_ref[subplot][1]].fill_between(x, normal_error[0], normal_error[1], color=COLOR_1, alpha=0.2)
+            axes[axes_ref[subplot][0]][axes_ref[subplot][1]].fill_between(x, fraud_error[0], fraud_error[1], color=RED_COLORS[1], alpha=0.2)
+
+    # layout 
+
+    plt.tight_layout(rect=[0, 0.03, 1, 1.05]) 
+
+    # tight_layout() only considers ticklabels, axis labels, and titles
+    # adjustment with [left bottom, right, top] in normalized (0,1) space
+    # increase top to reduce space between top figures and suptitle (that is created when customizing the legend)
     
     plt.show()
     return
